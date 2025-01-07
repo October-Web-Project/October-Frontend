e9d41625bab2981074f7cee59d4462acebf21924
BByungs <byungjin0120@gmail.com>
2025-01-07 23:14:41 +0900

    📦 chore: auth.js로 패키지 변경 및 적용, 유틸폴더 하위로 위치 변경

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,21 +1,25 @@
-import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 import NaverProvider from "next-auth/providers/naver";
 
 import { ENV } from "@/constants/env";
 
-export const authOptions: AuthOptions = {
+const getAuthorizationLink = (provider: "kakao" | "naver") => {
+  return `${ENV.API_URL}/oauth2/authorization/${provider}`;
+};
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
     KakaoProvider({
       clientId: ENV.KAKAO_CLIENT_ID,
       clientSecret: ENV.KAKAO_CLIENT_SECRET,
-      authorization: `${ENV.API_URL}/oauth2/authorization/kakao`,
+      authorization: getAuthorizationLink("kakao"),
     }),
     NaverProvider({
       clientId: ENV.NAVER_CLIENT_ID,
       clientSecret: ENV.NAVER_CLIENT_SECRET,
-      authorization: `${ENV.API_URL}/oauth2/authorization/naver`,
+      authorization: getAuthorizationLink("naver"),
     }),
   ],
   secret: ENV.NEXT_AUTH_SECRET,
-};
+});
