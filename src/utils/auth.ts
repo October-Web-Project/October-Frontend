d2e76aa047e6fffeac94a5cac93898fea417b50f
BByungs <byungjin0120@gmail.com>
2025-01-08 20:52:19 +0900

    ✨ feat: 공식 oauth 엔드포인트 url로 수정, params에 설정한 redirect_uri 넣어줌

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,8 +4,8 @@ import NaverProvider from "next-auth/providers/naver";
 
 import { ENV } from "@/constants/env";
 
-const getAuthorizationLink = (provider: "kakao" | "naver") => {
-  return `${ENV.API_URL}/oauth2/authorization/${provider}`;
+const getRedirectUri = (provider: "kakao" | "naver") => {
+  return `${ENV.NEXTAUTH_URL}/api/auth/callback/${provider}`;
 };
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
@@ -13,12 +13,26 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
     KakaoProvider({
       clientId: ENV.KAKAO_CLIENT_ID,
       clientSecret: ENV.KAKAO_CLIENT_SECRET,
-      authorization: getAuthorizationLink("kakao"),
+      authorization: {
+        url: "https://kauth.kakao.com/oauth/authorize",
+        params: {
+          response_type: "code",
+          client_id: ENV.KAKAO_CLIENT_ID,
+          redirect_uri: getRedirectUri("kakao"),
+        },
+      },
     }),
     NaverProvider({
       clientId: ENV.NAVER_CLIENT_ID,
       clientSecret: ENV.NAVER_CLIENT_SECRET,
-      authorization: getAuthorizationLink("naver"),
+      authorization: {
+        url: "https://nid.naver.com/oauth2.0/authorize",
+        params: {
+          response_type: "code",
+          client_id: ENV.NAVER_CLIENT_ID,
+          redirect_uri: getRedirectUri("naver"),
+        },
+      },
     }),
   ],
   secret: ENV.NEXT_AUTH_SECRET,
